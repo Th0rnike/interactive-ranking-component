1ff0679bb0f05a079348538c3ee5b3e5612022c0
Bald Eagle <beagle@gmail.com>
2023-12-13 15:13:58 +0400

    functionality is added. rating to catch rating and submit to check if submit is clicked. - both are states. now config css

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 import star from "./images/icon-star.svg";
+import thankYou from "./images/illustration-thank-you.svg";
 
 function App() {
+  const [rating, setRating] = useState<number>();
+  const [submit, setSubmit] = useState<boolean>(false);
+
+  const handleClick = (r: number) => {
+    setRating(r);
+  };
+
+  const submitRating = () => {
+    setSubmit(!submit);
+  };
+
   return (
     <div className="App">
-      <div>
-        <img src={star} alt="" />
-      </div>
-      <div>
-        <h1>How did we do?</h1>
-        <p>
-          Please let us know how we did with your support request. All feedback
-          is appreciated to help us improve our offering!
-        </p>
-      </div>
+      <div></div>
       <div className="buttons">
-        <div className="ranks">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-        </div>
-        <button>SUBMIT</button>
+        {rating && submit ? (
+          <>
+            <img src={thankYou} alt="" />
+            <h1>You selected {rating} of 5</h1>
+            <p>
+              We appreciate you taking the time to give a rating. If you ever
+              need more support, don’t hesitate to get in touch!
+            </p>
+          </>
+        ) : (
+          <>
+            <div>
+              <img src={star} alt="" />
+            </div>
+            <h1>How did we do?</h1>
+            <p>
+              Please let us know how we did with your support request. All
+              feedback is appreciated to help us improve our offering!
+            </p>
+            <div className="ranks">
+              {[1, 2, 3, 4, 5].map((r: number) => (
+                <button onClick={() => handleClick(r)} key={r}>
+                  {r}
+                </button>
+              ))}
+            </div>
+            <button onClick={submitRating}>Submit</button>
+          </>
+        )}
       </div>
     </div>
   );
