55316aa3a223454972ed65bdc812cf8256fd9984
Bald Eagle <beagle@gmail.com>
2023-12-13 14:53:18 +0400

    ready to add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import star from "./images/icon-star.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <img src={star} alt="" />
+      </div>
+      <div>
+        <h1>How did we do?</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="buttons">
+        <div className="ranks">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+        <button>SUBMIT</button>
+      </div>
     </div>
   );
 }
